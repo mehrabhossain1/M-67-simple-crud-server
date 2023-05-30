019b097d0f798a5fff1457326fdda5d1a7f57f81
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-30 22:51:48 +0600

    loaded single user data

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       console.log("Please delete from database", id);
